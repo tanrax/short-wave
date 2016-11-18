0f8bc8f44cdeefdcf1c2b858a39cf02cfeb7d553
Andros Fenollosa <andros@fenollosa.email>
2016-11-18 01:24:23 +0100

    Fix api mailgun

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def debug():
 
 def send_simple_message(html):
     return requests.post(
-        os.environ.get('GUNMAIL_URL'),
-        auth=("api", os.environ.get('GUNMAIL_API') + '/messages'),
+        os.environ.get('GUNMAIL_URL') + '/messages',
+        auth=("api", os.environ.get('GUNMAIL_API')),
         data={"from": os.environ.get('EMAIL_FROM'),
               "to": [os.environ.get('EMAIL_TO')],
               "subject": os.environ.get('SUBJECT'),
